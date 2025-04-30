33a1f3dcd3a0494190cebd0db3276f5ca01e7046
Shikha Badhan <162896164+Shikhabadhan01@users.noreply.github.com>
2025-04-30 16:42:58 +0530

    Update DBConnection.java

--- src/java/com/beautyparlour/db/DBConnection.java
+++ src/java/com/beautyparlour/db/DBConnection.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/beauty_parlour"; 
     private static final String USER = "root"; 
-    private static final String PASSWORD = "Shikha@8585"; 
+    private static final String PASSWORD = "your password"; 
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         Class.forName("com.mysql.cj.jdbc.Driver"); // Load MySQL Driver
         return DriverManager.getConnection(URL, USER, PASSWORD);
